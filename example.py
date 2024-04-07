44fec392fd4e03df18b96e6050d173bcf5e21dc0
Tetsui Ohkubo <peryaudo@gmail.com>
2024-04-07 16:35:53 -0700

    Calculate val loss

--- example.py
+++ example.py
@@ -19,22 +19,40 @@ model = Model()
 
 optimizer = doja.SGD(model.parameters, lr=3e-4)
 
+def format_dataset(dataset):
+    MEAN = 0.1307
+    STD = 0.3081
+    images = dataset['image'].astype(np.float32)
+    images = (images / 255.0 - MEAN) / STD
+    images = images.reshape(-1, 28 * 28)
+    labels = dataset['label']
+    labels =  np.identity(10, dtype=np.float32)[labels]
+    return images, labels
+
 dataset = load_dataset("mnist").with_format('numpy')
-train_dataset = dataset['train']
+train_images, train_labels = format_dataset(dataset['train'])
+val_images, val_labels = format_dataset(dataset['test'])
 
-MEAN = 0.1307
-STD = 0.3081
-train_images = train_dataset['image'].astype(np.float32)
-train_images = (train_images / 255.0 - MEAN) / STD
-train_images = train_images.reshape(-1, 28 * 28)
-train_labels = train_dataset['label']
-train_labels =  np.identity(10, dtype=np.float32)[train_labels]
+for epoch_idx in range(100):
+    for i in range(0, train_images.shape[0], 16):
+        images = doja.Tensor(train_images[i:i+16])
+        labels = doja.Tensor(train_labels[i:i+16])
+        logits = model(images)
+        loss = logits.cross_entropy(labels)
+        loss.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+    val_loss = 0
+    num_batches = 0
 
-for i in range(0, len(train_dataset), 16):
-    images = doja.Tensor(train_images[i:i+16])
-    labels = doja.Tensor(train_labels[i:i+16])
-    logits = model(images)
-    loss = logits.cross_entropy(labels)
-    loss.zero_grad()
-    loss.backward()
-    optimizer.step()
+    for i in range(0, val_images.shape[0], 16):
+        images = doja.Tensor(val_images[i:i+16])
+        labels = doja.Tensor(val_labels[i:i+16])
+        logits = model(images)
+        loss = logits.cross_entropy(labels)
+        val_loss += float(loss.data)
+        num_batches += 1
+    
+    val_loss /= num_batches
+    print("val loss after epoch {}: {:.4f}".format(epoch_idx, val_loss))
